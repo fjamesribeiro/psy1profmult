efa7b77893fa0c29dc76521ec8d26b0556bd9a24
Seu Nome <seu.email@example.com>
2025-10-24 17:13:54 -0300

    ajustes

---
+++ app/utils/gerasenha.py
@@ -0,0 +1,8 @@
+from app.services.auth import get_password_hash
+
+# Gere o hash da senha
+senha_hash = get_password_hash("psyconexa")
+print(senha_hash)
+
+# Atualize no banco manualmente ou via script:
+# UPDATE profissional SET senha_hash = 'hash_gerado' WHERE id = 1;
